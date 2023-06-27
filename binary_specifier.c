b57143ef5d6fa36d8d0d55b1569670261fc04210
JessicaChideraOgu <ogujessica3@gmail.com>
2023-06-27 15:33:59 +0100

    Updated binary_specifier.c

--- binary_specifier.c
+++ binary_specifier.c
@@ -7,33 +7,33 @@
  */
 int print_b(va_list jes_dre)
 {
-        unsigned int i, count, Num, binary, arr[32];
+	unsigned int i, count, Num, binary, arr[32];
 
-        i = 0, count = 0;
-        Num = va_arg(jes_dre, int);
+	i = 0, count = 0;
+	Num = va_arg(jes_dre, int);
 
-        if (Num < 1)
-        {
-                _putchar(48);
-                count++;
-                return (count);
-        }
-        else
-        {
-                while (Num > 0)
-                {
-                        binary = Num % 2;
-                        Num /= 2;
-                        arr[count] = binary;
-                        count++;
-                }
-                i = count - 1;
-                while (i > 0)
-                {
-                        _putchar('0' + arr[i]);
-                        i--;
-                }
-                _putchar('0' + arr[i]);
-        }
-        return (count);
+	if (Num < 1)
+	{
+	_putchar(48);
+	count++;
+	return (count);
+	}
+	else
+	{
+	while (Num > 0)
+	{
+	binary = Num % 2;
+	Num /= 2;
+	arr[count] = binary;
+	count++;
+	}
+	i = count - 1;
+	while (i > 0)
+	{
+	_putchar('0' + arr[i]);
+	i--;
+	}
+	_putchar('0' + arr[i]);
+	}
+	return (count);
 }
