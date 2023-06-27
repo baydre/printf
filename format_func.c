f0afc0d9a6858871be33eb1e6c5e10e7b3666f80
JessicaChideraOgu <ogujessica3@gmail.com>
2023-06-27 15:16:01 +0100

    Updated format_func.c file

--- format_func.c
+++ format_func.c
@@ -6,21 +6,21 @@
  */
 int (*get_func(char s))(va_list)
 {
-        bj_t bjs[] = {
-                {"c", print_c},
-                {"s", print_s},
-                {"d", print_d},
-                {"i", print_i},
-                {"b", print_b},
-                {NULL, NULL}
-        };
+	bj_t bjs[] = {
+	{"c", print_c},
+	{"s", print_s},
+	{"d", print_d},
+	{"i", print_i},
+	{"b", print_b},
+	{NULL, NULL}
+	};
 
-        int i;
+	int i;
 
-        for (i = 0; bjs[i].jess != NULL; i++)
-        {
-                if (*bjs[i].jess == s)
-                        return (bjs[i].bay);
-        }
-        return (NULL);
+	for (i = 0; bjs[i].jess != NULL; i++)
+	{
+	if (*bjs[i].jess == s)
+	return (bjs[i].bay);
+	}
+	return (NULL);
 }
