5a55f9c96b49efac34303560d23df9ee912bf884
JessicaChideraOgu <ogujessica3@gmail.com>
2023-06-27 15:57:52 +0100

    Updated custom_printf.c

--- custom_printf.c
+++ custom_printf.c
@@ -7,42 +7,42 @@
  */
 int _printf(const char *format, ...)
 {
-        int count = -1;
+	int count = -1;
 
-        if (format != NULL)
-        {
-                int i;
-                va_list baydre;
-                int (*o)(va_list);
+	if (format != NULL)
+	{
+	int i;
+	va_list baydre;
+	int (*o)(va_list);
 
-                va_start(baydre, format);
+	va_start(baydre, format);
 
-                if (format[0] == '%' && format[1] == '\0')
-                        return (-1);
+	if (format[0] == '%' && format[1] == '\0')
+	return (-1);
 
-                count = 0;
+	count = 0;
 
-                for (i = 0; format[i] != '\0'; i++)
-                {
-                        if (format[i] == '%')
-                        {
-                                if (format[i + 1] == '%')
-                                {
-                                        count += _putchar(format[i]);
-                                        i++;
-                                }
-                                else if (format[i + 1] != '\0')
-                                {
-                                        o = get_func(format[i + 1]);
-                                        count += (o ? o(baydre) : _putchar(format[i]) + _putchar(format[i + 1]));
-                                        i++;
-                                }
-                        }
-                        else
-                                count += _putchar(format[i]);
-                }
-                va_end(baydre);
-        }
+	for (i = 0; format[i] != '\0'; i++)
+	{
+	if (format[i] == '%')
+	{
+	if (format[i + 1] == '%')
+	{
+		count += _putchar(format[i]);
+		i++;
+	}
+	else if (format[i + 1] != '\0')
+		{
+			o = get_func(format[i + 1]);
+			count += (o ? o(baydre) : _putchar(format[i]) + _putchar(format[i + 1]));
+			i++;
+		}
+	}
+	else
+		count += _putchar(format[i]);
+	}
+		va_end(baydre);
+	}
 
-        return (count);
+	return (count);
 }
