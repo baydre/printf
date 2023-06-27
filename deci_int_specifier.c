711dda3237e44cccc54bb806123134941a80f86d
JessicaChideraOgu <ogujessica3@gmail.com>
2023-06-27 15:42:57 +0100

    Updated deci_int_specifier.c

--- deci_int_specifier.c
+++ deci_int_specifier.c
@@ -6,34 +6,35 @@
  */
 int print_d(va_list baydre)
 {
-        unsigned int num_Absolute, aux_Num, count_Zero, count;
+	unsigned int num_Absolute, aux_Num, count_Zero, count;
 
-        int numbers;
-        count = 0;
-        numbers = va_arg(baydre, int);
+	int numbers;
 
-        if (numbers < 0)
-        {
-                num_Absolute = (numbers * -1);
-                count += _putchar(45);
-        }
-        else
-                num_Absolute = numbers;
+	count = 0;
+	numbers = va_arg(baydre, int);
 
-        aux_Num = num_Absolute;
-        count_Zero = 1;
-        while (aux_Num > 9)
-        {
-                aux_Num /= 10;
-                count_Zero *= 10;
-        }
+	if (numbers < 0)
+	{
+	num_Absolute = (numbers * -1);
+	count += _putchar(45);
+	}
+	else
+		num_Absolute = numbers;
 
-        while (count_Zero >= 1)
-        {
-                count += _putchar(((num_Absolute / count_Zero) % 10) + '0');
-                count_Zero /= 10;
-        }
-        return (count);
+		aux_Num = num_Absolute;
+		count_Zero = 1;
+	while (aux_Num > 9)
+	{
+	aux_Num /= 10;
+	count_Zero *= 10;
+	}
+
+	while (count_Zero >= 1)
+	{
+		count += _putchar(((num_Absolute / count_Zero) % 10) + '0');
+		count_Zero /= 10;
+	}
+	return (count);
 }
 /**
  * print_i - Print the integers (%i)
@@ -42,5 +43,5 @@ int print_d(va_list baydre)
  */
 int print_i(va_list baydre)
 {
-        return (print_d(baydre));
+	return (print_d(baydre));
 }
