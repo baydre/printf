7f106a3ba4018736ac718dd9d66fa06f465c6bd1
JessicaChideraOgu <ogujessica3@gmail.com>
2023-06-27 15:06:04 +0100

    Updated main.h file

--- main.h
+++ main.h
@@ -14,9 +14,16 @@
 
 typedef struct bj
 {
-        char *jess;
-        int (*bay)(va_list);
+	char *jess;
+	int (*bay)(va_list);
 } bj_t;
-
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_c(va_list baydre);
+int (*format_func(char s))(va_list baydre);
+int print_s(va_list baydre);
+int print_d(va_list baydre);
+int print_i(va_list jessie);
+int print_b(va_list jes_dre);
 
 #endif
